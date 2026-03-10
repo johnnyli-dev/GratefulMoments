6041fc3c0446a85fc1251b028c4dd57cc2deb627
Johnny Li <jql1227@gmail.com>
2026-03-10 15:36:40 -0700

    Initial Commit

---
+++ Grateful Moments/Grateful_MomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grateful_MomentsApp.swift
+//  Grateful Moments
+//
+//  Created by Johnny Li on 3/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Grateful_MomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
