58bb626422f84759de53977a02ba2585df627a06
Johnny Li <jql1227@gmail.com>
2026-03-19 13:43:04 -0700

    custom UI

---
+++ Grateful Moments/Tabs/Moments/MomentsView.swift
@@ -0,0 +1,67 @@
+//
+//  MomentsView.swift
+//  Grateful Moments
+//
+//  Created by Johnny Li on 3/16/26.
+//
+
+import SwiftUI
+import SwiftData
+
+
+struct MomentsView: View {
+    @State private var showCreateMoment = false
+    @Query(sort: \Moment.timestamp)
+    private var moments: [Moment]
+
+
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                pathItems.frame(maxWidth: .infinity)
+            }
+            .overlay {
+                if moments.isEmpty {
+                    ContentUnavailableView {
+                        Label("No moments yet!", systemImage: "exclamationmark.circle.fill")
+                    } description: {
+                        Text("Post a note or photo to start filling this space with gratitude.")
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement : .primaryAction) {
+                    Button {
+                       showCreateMoment = true
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    .sheet(isPresented: $showCreateMoment) {
+                        MomentEntryView()
+                    }
+                }
+            }
+            .navigationTitle("Grateful Moments")
+        }
+    }
+    private var pathItems: some View {
+        ForEach(moments) {moment in
+            NavigationLink {
+                MomentDetailView(moment: moment)
+            } label: {
+                Text(moment.title)
+            }
+        }
+    }
+    
+}
+
+
+#Preview {
+    MomentsView()
+        .sampleDataContainer()
+}
+
+#Preview("No mements") {
+    MomentsView().modelContainer(for: [Moment.self])
+}
